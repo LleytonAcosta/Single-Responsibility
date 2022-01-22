2b68c848297fdb7d2aa89a151f6f65f540d64fcc
LleytonAcosta <Lleyton_200206@hotmail.com>
2022-01-21 19:14:13 -0500

    Agregar archivos de proyecto.

---
+++ Item.cs
@@ -0,0 +1,23 @@
+﻿namespace Single_Responsibility
+{
+    public class Item
+    { // Propiedades 
+        public Producto Producto { get; set; }
+
+        public int Cantidad { get; set; }
+
+        //Constructor 
+        public Item(Producto producto, int cantidad)
+        {
+            Producto = producto;
+            Cantidad = cantidad;
+        }
+
+        // Metodo que se aplica solo la responsabilidad de calcular el subtotal de los productos
+        public double Subtotal()
+        {
+            return Cantidad * Producto.Precio;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Single_Responsibility
+{
+    class Program
+    {
+
+     
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("*************Patron de diseño Single Responsibility ************* ");
+            // Datos del cliente
+            Cliente cliente = new Cliente();
+            cliente.Apellido = " Acosta Pincay";
+            cliente.Nombre = "Lleyton Jaime ";
+
+            // Datos que se implementa a la factura
+            Factura factura = new Factura(1314978, cliente);
+
+            Item item_1 = new Item(new Producto("Pan",5), 5);
+
+            Item item_3 = new Item(new Producto("Aceite",100), 2);
+
+            Item item_2 = new Item(new Producto("Jugo", 50), 3);
+
+            // Items o productos 
+            factura.Items.Add(item_1);
+            factura.Items.Add(item_2);
+            factura.Items.Add(item_3);
+
+            // Se muestra la factura
+            Console.WriteLine("");
+            Console.WriteLine("Fecha:" + factura.Fecha);
+            Console.WriteLine("");
+            Console.WriteLine("Cedula:" + factura.Cedula);
+            Console.WriteLine("");
+            Console.WriteLine("Cliente:" + cliente.Nombre + cliente.Apellido);
+            Console.WriteLine("");
+            Console.WriteLine("Toltal de la comra: $"+factura.Total());
+        }  
+       
+    }
+}
